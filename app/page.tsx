31ceda9ce2a76ec1be47e79f7925cd5b3eff5e1c
Raiyan708 <97243322+Raiyan708@users.noreply.github.com>
2025-05-08 15:04:53 +0530

    Updated image path for Mazir Abbasi

--- app/page.tsx
+++ app/page.tsx
@@ -80,11 +80,11 @@ export default function Home() {
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                    Premier IAS Coaching in Aligarh, Led by Expert Delhi Faculty
+                    Premier IAS Coaching in Aligarh, Led by Expert Faculties from Delhi
                   </h1>
                   <p className="text-lg italic">Where private coaching meets personalized tuition</p>
                   <p className="max-w-[600px] text-muted-foreground md:text-xl">
-                    Expert guidance from experienced Delhi faculty, right here in Aligarh. Your path to UPSC, UPPSC,
+                    Expert guidance from experienced Faculties from Delhi, right here in Aligarh. Your path to UPSC, UPPSC,
                     BPSC, and APSC success starts at Care IAS.
                   </p>
                 </div>
